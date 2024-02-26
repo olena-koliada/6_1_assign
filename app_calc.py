3e3c50671704809c3f90029f64602f38687af86e
Olena K <olena.kolyada@gmail.com>
2024-02-26 16:13:51 +0100

    Add multiplication option

--- app_calc.py
+++ app_calc.py
@@ -3,14 +3,15 @@ def calculator(a, b, operation):
         return a + b
     elif operation == "-":
         return a - b
+    elif operation == "*":
+        return a * b
     else:
-        print("Invalid operation. Please enter '+' or '-': ")
+        print("Invalid operation. Please enter '+', '-'or '*': ")
 
 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-operation = input("Choose an operation (+ or -): ")
-
+operation = input("Choose an operation (+, - or *): ")
 
 result = calculator(a, b, operation)
 
