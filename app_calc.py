9f943c7abe253678085ced5a50f21bc72be66f13
Olena K <olena.kolyada@gmail.com>
2024-02-26 15:40:10 +0100

    addition and substraction

---
+++ app_calc.py
@@ -0,0 +1,17 @@
+def calculator(a, b, operation):
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    else:
+        print("Invalid operation. Please enter '+' or '-': ")
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+operation = input("Choose an operation (+ or -): ")
+
+
+result = calculator(a, b, operation)
+
+print(f"The result is: {result}")
